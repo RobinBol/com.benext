bf8132b2d47abbd92e8da611db49f00539e78508
Dave <simmer1974@gmail.com>
2016-09-21 21:43:25 +0200

    update

--- drivers/p1-dongle/driver.js
+++ drivers/p1-dongle/driver.js
@@ -1,36 +1,74 @@
-"use strict";
+'use strict';
 
 const path = require('path');
 const ZwaveDriver = require('homey-zwavedriver');
 
-// http://www.cd-jackson.com/index.php/zwave/zwave-device-database/zwave-device-list/devicesummary/340
+// // http://www.cd-jackson.com/index.php/zwave/zwave-device-database/zwave-device-list/devicesummary/340
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 debug: true,
 	capabilities: {
-		'measure_power': {
-				'command_class': 'COMMAND_CLASS_METER',
-                    'command_report': 'METER_REPORT',
-                    'command_report_parser': function (report) {
-                        return report['Meter Value (Parsed)'];
-                    }
-                },
+		measure_power: {
+				command_class: 'COMMAND_CLASS_METER',
+				command_get: 'METER_GET',
+				command_get_parser: function () {
+					return {
+						Properties1: {
+							'Rate Type': 1,
+						},
+					};
+				},
+				command_report: 'METER_REPORT',
+				command_report_parser: report => {
+					if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 2) {
+						return report['Meter Value (Parsed)'];
+					} return null;
+				},
+		},
 
-		'meter_power': {
-			'command_class': 'COMMAND_CLASS_METER',
-                    'command_report': 'METER_REPORT',
-                    'command_report_parser': function (report) {
-                        return report['Meter Value (Parsed)'];
-                    }
+		meter_power: {
+			command_class: 'COMMAND_CLASS_METER',
+			command_get: 'METER_GET',
+			command_get_parser: function () {
+				return {
+					Properties1: {
+						Scale: 0,
+					},
+				};
 			},
-		'meter_gas': {
-			'command_class': 'COMMAND_CLASS_METER',
-                    'command_report': 'METER_REPORT',
-                    'command_report_parser': function (report) {
-                        return report['Meter Value (Parsed)'];
-                    }
-			}
+			command_report: 'METER_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Properties2')
+				&& report.Properties2.hasOwnProperty('Scale bits 10')
+				&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				} return null;
+			},
+		},
+
+		meter_gas: {
+			command_class: 'COMMAND_CLASS_METER',
+			//command_get: 'METER_GET',
+			command_get_parser: function () {
+				return {
+					Properties1: {
+						Scale: 0,
+					},
+				};
+			},
+			command_report: 'METER_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Properties2')
+				&& report.Properties2.hasOwnProperty('Scale bits 10')
+				&& report.Properties2['Scale bits 10'] === 0) {
+				return report['Meter Value (Parsed)'];
+				} return null;
+			},
+		},
 	},
+
 	settings: {
 	}
 });
